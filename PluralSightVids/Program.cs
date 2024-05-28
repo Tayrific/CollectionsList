97292389be5f0613cf2439566ebed574d7c4f463
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-05-28 11:41:50 +0100

    Working on list

--- PluralSightVids/Program.cs
+++ PluralSightVids/Program.cs
@@ -41,11 +41,73 @@
             string chosenDay = daysOfWeek[number - 1]; //array index starts at 0, so subtract 1 from the number entered by the user
             Console.WriteLine("your chosen day is {0}", chosenDay); //display the chosen day
 
-            Console.ReadLine();
-
             Console.WriteLine("------------------");
 
-            
+            int[] ints = new int[5]; //array of 5 integers without knowing elements that will be in the array
+
+            List<String> days = new List<String>(); //list of strings, can add or remove elements
+            List<int> numvers = new List<int>(); 
+
+            days.Add("Monday"); //add elements to the list
+            days.Add("Tuesday");
+            days.Add("Wednesday");
+            days.Add("Thursday");
+            days.Add("Friday");
+            days.Add("Saturday");
+            days.Add("Sunday");
+            days.Add("day");
+
+            int count = days.Count; //count the number of elements in the list
+
+            days.Remove("day"); //remove element from the list
+
+            days.Insert(4, "randday"); //insert element at a specific index
+
+            days.RemoveAt(2); //remove element at a specific index
+
+            days.Sort(); //sort elements in the list
+
+            Console.WriteLine("-----------------------"); 
+            Console.WriteLine("\n-----sorted array-----");
+
+            foreach (var day in days)
+            {
+                Console.WriteLine(day);
+            }
+
+
+            days.Reverse(); //reverse the order of elements in the list
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("\n----reversed array----");
+
+            foreach (var day in days)
+            {
+                Console.WriteLine(day);
+            }
+
+            days.FindIndex(x => x == "saturday"); //find the index of an element in the list
+
+
+            bool sat = days.Contains("Saturday"); //check if an element is in the list
+
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("\n----check if saturday is in the list----");
+            if (sat)
+            {
+                Console.WriteLine("Saturday is in the list");
+            }
+       
+
+            days.Clear(); //remove all elements from the list
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("\n----clear list----");
+            if (days.Count == 0)
+            {
+                Console.WriteLine("List is empty");
+            }
         }     
     }
 }
