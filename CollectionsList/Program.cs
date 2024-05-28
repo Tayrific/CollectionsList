97eff9161b0715af9e8ccedf5f173db56fa712b8
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-05-28 10:20:03 +0100

    Add project files.

---
+++ CollectionsList/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CollectionsList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Names = new List<string>();
+            Names.Add("Joe");
+            Names.Add("Bob");
+            Names.Add("Jack");
+            Names.Add("Steve");
+            
+            foreach (var name in Names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("------------------");
+
+            var Numbers = new List<int>() {0,1,2,3,4,5,6,7,8,9,10};
+            foreach (var number in Numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ListExercise/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace ListExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var objs = new List<object>();
+
+            objs.Add(2.5d);
+            objs.Add(2.0d);
+            objs.Add(3);
+            objs.Add(2);
+            objs.Add("Hello");
+            objs.Add("World");
+
+            int intSum = 0;
+            double doubleSum = 0;          
+            StringBuilder stringConcat = new StringBuilder();
+
+            foreach (var obj in objs)
+            {
+                string type = obj.GetType().ToString();
+                Console.WriteLine(type);
+
+                if (obj is double doubleVal)
+                {
+                    doubleSum += doubleVal;
+                }
+                else if (obj is int intVal)
+                {
+                    intSum += intVal;
+                }
+                else if (obj is string stringVal)
+                {
+                    stringConcat.Append((stringVal) + " ");               
+                } 
+                else
+                {
+                   Console.WriteLine("Unknown type: {0}", type);
+                }
+            
+            }
+            Console.WriteLine("Sum of ints: {0}", intSum);
+            Console.WriteLine("Sum of doubles: {0}", doubleSum);       
+            Console.WriteLine("Concatenated string: {0}", stringConcat);
+        }
+    }
+}
